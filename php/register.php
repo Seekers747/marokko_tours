abf266bf7274a9dba59d9ef27b4e098cbaf22341
unknown <imraan.mohamedhoesein@student.rocva.nl>
2025-01-25 22:17:03 +0100

    register css fixed and small issue on scroll behaviour fixed

--- php/register.php
+++ php/register.php
@@ -21,27 +21,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Compact Form</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="../css/reg-log.css">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="../css/general.css">
+        <link rel="stylesheet" href="../css/reg-log.css">
+
     </head>
 
     <body>
         <nav class="navbar-adjustment">
             <p>Website logo/name</p>
-            <ul class="nav nav-pills">
+            <ul class="nav nav-pills nav-spacing">
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="../html/main-pages/home.html">Home</a>
+                    <a class="nav-link button-nav" aria-current="page" href="../html/main-pages/home.html">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="../html/main-pages/over-ons.html">About us</a>
+                    <a class="nav-link button-nav" href="../html/main-pages/over-ons.html">About us</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="../html/main-pages/hoe-het-werkt.html">How it works</a>
+                    <a class="nav-link button-nav" href="../html/main-pages/hoe-het-werkt.html">How it works</a>
                 </li>
             </ul>
             <div class="log-reg">
-                <button type="button" class="btn btn-primary"><a href="login.php" class="login-button">Login</a></button>
-                <button type="button" class="btn btn-primary"><a href="register.php" class="signup-button">Sign up</a></button>
+                <button type="button" class="btn button-nav-active button-nav"><a href="login.php" class="login-button">Login</a></button>
+                <button type="button" class="btn button-nav-active button-nav"><a href="register.php" class="signup-button">Sign up</a></button>
             </div>
         </nav>
         <div class="body_for_form">
@@ -81,7 +86,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         </label>
                     </div>
 
-                    <button type="submit" class="btn btn-primary w-100">Submit</button>
+                    <button type="submit" class="btn button-nav-active button-nav w-100">Submit</button>
                 </form>
             </div>
         </div>   
