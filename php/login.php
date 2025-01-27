4519e63b96889fdf48108257d4f35b0171d8394a
unknown <imraan.mohamedhoesein@student.rocva.nl>
2025-01-27 08:11:35 +0100

    removed useless file

--- php/login.php
+++ php/login.php
@@ -23,7 +23,7 @@ require "../data/user.php";
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="../css/reg-log.css">
         <link rel="stylesheet" href="../css/general.css">
-        <link rel="stylesheet" href="../css/login.css">
+        <style type="text/css">.empty_space {height: 10px;}</style>
     </head>
 
     <body>
